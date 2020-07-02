4c98d09ae2a4594f6947d78a0fc0149701536b4d
trever <toakumu@gmail.com>
2020-07-02 10:02:25 +0300

    changes

--- user_credentials.py
+++ user_credentials.py
@@ -2,7 +2,7 @@ import pyperclip
 import random
 import string
 
-# Global Variables
+# Global variables
 global users_list 
 class User:
 	'''
@@ -10,4 +10,14 @@ class User:
 	'''
 	# Class Variables
 	# global users_list
-    
+    users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
+
