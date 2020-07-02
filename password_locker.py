b71869774b9a55e7cb58a696d8a2b1b63d10f4b2
trever <toakumu@gmail.com>
2020-07-02 09:51:40 +0300

    added elif function

--- password_locker.py
+++ password_locker.py
@@ -64,7 +64,7 @@ def copy_credential(site_name):
 		short_code = input('Enter a choice: ').lower().strip()
 		if short_code == 'ex':
 			break
-        
+
         elif short_code == 'ca':
     			print("-"*60)
 			print(' ')
@@ -75,3 +75,31 @@ def copy_credential(site_name):
 			save_user(create_user(first_name,last_name,password))
 			print(" ")
 			print(f'New Account Created for: {first_name} {last_name} using password: {password}')
+
+            elif short_code == 'li':
+    			print("-"*60)
+			print(' ')
+			print('To login, enter your account details:')
+			user_name = input('Enter your first name - ').strip()
+			password = str(input('Enter your password - '))
+			user_exists = verify_user(user_name,password)
+			if user_exists == user_name:
+				print(" ")
+				print(f'Welcome {user_name}. Please choose an option to continue.')
+				print(' ')
+				while True:
+					print("-"*60)
+					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					print("-"*60)
+					if short_code == 'ex':
+						print(" ")
+				while True:
+					print("-"*60)
+					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					print("-"*60)
+					if short_code == 'ex':
+						print(" ")
+						print(f'Goodbye {user_name}')
+						break
