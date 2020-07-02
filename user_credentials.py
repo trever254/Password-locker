96e24c159a9cff624e157cc230641cdcfb98a089
trever <toakumu@gmail.com>
2020-07-02 10:08:14 +0300

    add __int__ function

--- user_credentials.py
+++ user_credentials.py
@@ -27,3 +27,31 @@ class User:
 		'''
 		User.users_list.append(self)
 
+class Credential:
+    	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	# Class Variables
+	credentials_list =[]
+	user_credentials_list = []
+	@classmethod
+	def check_user(cls,first_name,password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+
+        def __init__(self,user_name,site_name,account_name,password):
+    		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
