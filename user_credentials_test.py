2cd58c4c184fa613cb7405322da210bfb32e79fd
trever <toakumu@gmail.com>
2020-07-02 10:25:51 +0300

    added TestUser

---
+++ user_credentials_test.py
@@ -0,0 +1,11 @@
+import unittest
+import pyperclip
+from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+    	'''
+	Test class that defines test cases for the user class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
