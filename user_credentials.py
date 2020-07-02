df05a92b3b1ce87b5544be11cb55031d8ab9a7fd
trever <toakumu@gmail.com>
2020-07-02 10:00:42 +0300

    added global users list

---
+++ user_credentials.py
@@ -0,0 +1,13 @@
+import pyperclip
+import random
+import string
+
+# Global Variables
+global users_list 
+class User:
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+    
